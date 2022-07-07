c8d573787df7803d671144ba28b314fa57b779ef
Intigam <intigam.mammadli@gmail.com>
2022-07-07 12:24:33 +0400

    SQL task 1 added

---
+++ Python-tasks/SQL/movie_db.py
@@ -0,0 +1,20 @@
+import psycopg2
+
+conn = psycopg2.connect(user="postgres", password = "pg@dmin", host = "localhost", port = "5432", database = "movie_db")
+cursor = conn.cursor()
+
+query = """
+    CREATE TABLE film (
+        id SERIAL PRIMARY KEY,
+        name VARCHAR(50) NOT NULL,
+        description TEXT,
+        views INTEGER DEFAULT 0,
+        category_id INTEGER NOT NULL,
+        created_date TIMESTAMP NOT NULL,
+        has_fragmant BOOLEAN DEFAULT FALSE,
+    );
+
+"""
+
+cursor.execute(query)
+conn.commit()
